78d647962b7ba3ff305e56ef0a14754f39742b4b
Daniel Kandie <danielkandie14@gmail.com>
2025-07-20 19:37:55 +0300

    Task 1: Set up navigation with Header and routing between Home, Posts, and Users

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 
 const PostsPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl mb-4">Posts Page</h1>
-      <PostCard />
+    <div>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-3xl mb-4">Posts Page</h1>
+        <PostCard />
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import Header from "@/components/layout/Header";
 
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl mb-4">Users Page</h1>
-      <p>List of users will go here...</p>
+    <div>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-3xl mb-4">Users Page</h1>
+        <p>List of users will go here...</p>
+      </main>
     </div>
   );
 };
